c3d652c63511815605c8d2f0f5c921a840ee64e6
Apinda Tekula <nerdcod3rx@gmail.com>
2025-01-14 09:23:13 +0000

    I have completed the file metadata microservice

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
 
+const multer = require('multer');
+const path = require('path');
+const upload = multer({dest: 'uploads/'});
 var app = express();
 
 app.use(cors());
@@ -9,6 +12,18 @@ app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post("/api/fileanalyse/", upload.single('upfile'), function(req, res){
+    // console.log(req.file);
+    const name = req.file.originalname;
+    const filetype = req.file.mimetype;
+    const size = req.file.size;
+
+    res.json({name: `${name}`,
+    type: `${filetype}`,
+      size: parseInt(size)
+     });
 });
 
 
